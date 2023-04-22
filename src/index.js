377b3358286058df888ada79211053091de24460
CedricGuette <cedric.guette@gmail.com>
2023-04-22 20:08:19 +0200

    Added bac reactstictmode

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ const rootElement = document.getElementById('root')
 if(rootElement) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
+    <React.StrictMode>  
       <Router>
-          <App />
-      </Router>,
+        <App />
+      </Router>
+    </React.StrictMode>
   )
 }
